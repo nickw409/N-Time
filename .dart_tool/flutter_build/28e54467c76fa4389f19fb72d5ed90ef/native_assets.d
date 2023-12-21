4b9cc5c5c48639c6ea56e1373ce13d8d3f590008
Nick Wiley <nwiley333@gmail.com>
2023-12-21 12:38:06 -0800

    Init Flutter App

---
+++ .dart_tool/flutter_build/28e54467c76fa4389f19fb72d5ed90ef/native_assets.d
@@ -0,0 +1,1 @@
+ /Users/nick/dev/projects/N-Time/.dart_tool/flutter_build/28e54467c76fa4389f19fb72d5ed90ef/native_assets.yaml: 
---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'N-Time',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'N-Time'),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var status = false;
+
+  void getStatus() async {
+    final response = await http.get(
+                            Uri.parse('http://www.enginick.com:9696/status'));
+    
+    if (response.statusCode == 200) {
+      status = true;
+    } else {
+      status = false;
+    }
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.headlineMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(
+          widget.title,
+          style: style,
+        ),
+      ),
+      body: const Column(
+        children: [
+          Center(
+            child: Calendar(),
+          ),
+          SizedBox(height: 20,),
+          Center(
+            child: ServerStatus(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Calendar extends StatefulWidget {
+  const Calendar({super.key});
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      firstDay: DateTime.utc(2020, 1, 1),
+      lastDay: DateTime.utc(2030, 1, 1),
+      focusedDay: DateTime.now(),
+    );
+  }
+}
+
+class ServerStatus extends StatelessWidget {
+  const ServerStatus({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var status = appState.status;
+
+    appState.getStatus();
+
+    return Text("$status");
+  }
+}
